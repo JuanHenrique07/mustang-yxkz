98b74e3a44b1972c662236533ff97f7cac05e758
yxkz <135544577+JuanHenrique07@users.noreply.github.com>
2024-08-26 14:25:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+AOS.init();
+
+
+const menu = document.querySelector(".menu-mobile-content")
+
+function openMenu(){
+    menu.style.display="flex"
+}
+
+function closeMenu(){
+    menu.style.display="none"
+}
